904c932a625735ca04568b0aa7e9408ab1282651
FranciscoSoares0 <fsoares751@gmail.com>
2025-04-15 14:50:42 +0100

    Remove environment.ts from version control

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    apiBaseUrl: 'http://localhost:3000',
-    CLOUDINARY_UPLOAD_PRESET : 'projects_images_upload',
-    CLOUDINARY_URL : 'https://api.cloudinary.com/v1_1/duxvid3cd/image/upload',
-};
