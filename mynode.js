20c22268ecbc7e5d8ae403a75857e8d2b5b73cc9
FranciscoSoares0 <fsoares751@gmail.com>
2025-04-29 14:39:42 +0100

    deploy

--- mynode.js
+++ mynode.js
@@ -10,12 +10,12 @@ const envFile = `export const environment = {
     CLOUDINARY_URL='${process.env.CLOUDINARY_URL}',
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
     }
 });
