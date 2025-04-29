5454cf633f3505d56748af221f0c5d6399501553
FranciscoSoares0 <fsoares751@gmail.com>
2025-04-29 13:47:48 +0100

    deploy

---
+++ mynode.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
+const envFile = `export const environment = {
+    apiBaseUrl='${process.env.apiBaseUrl}',
+    CLOUDINARY_UPLOAD_PRESET='${process.env.CLOUDINARY_UPLOAD_PRESET}',
+    CLOUDINARY_URL='${process.env.CLOUDINARY_URL}',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+    }
+});
